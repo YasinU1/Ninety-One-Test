1e512bf06272f13e1459123967beb42e9dfbc2df
sayedmuhammadyasin-usman <sayedmuhammadyasin.usman@vodafone.com>
2023-09-18 22:09:44 +0100

    refactor: changed code formatting

--- main.py
+++ main.py
@@ -14,14 +14,24 @@
 def read_csv_file(filename):
     """Read the content of a CSV file."""
 
-    with open(filename, 'r') as file:
+    with open(filename, 'r') as file: # Reads the CSV file
         return file.read()
 
 def parse_csv_data(csv_data):
     """Turns CSV data into a dictionary of names and scores."""
-   
-    lines = csv_data.strip().split("\n")[1:]  # Skip the header
-    return {f"{first_name.strip()} {second_name.strip()}": int(score) for first_name, second_name, score in (line.split(",") for line in lines)}
+    # Split the CSV data by lines, and skip the header row.
+    lines = csv_data.strip().split("\n")[1:] 
+
+    result = {}
+    for line in lines:
+        # Split the line by commas to get individual fields
+        first_name, second_name, score = line.split(",")
+
+        # Strip any leading or trailing whitespace from the names and score
+        full_name = f"{first_name.strip()} {second_name.strip()}"
+        result[full_name] = int(score.strip())
+
+    return result
 
 
 
@@ -29,7 +39,13 @@ def get_top_scorers(scores_dict):
     """Get the top scorers from a dictionary of names and scores."""
     
     top_score = max(scores_dict.values())
-    return sorted(name for name, score in scores_dict.items() if score == top_score)
+    top_scorers = []
+
+    for name, score in scores_dict.items():
+        if score == top_score:
+            top_scorers.append(name)
+
+    return sorted(top_scorers)
 
 
 
